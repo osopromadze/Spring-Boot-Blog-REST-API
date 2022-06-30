7427f6c4d2988ea507519ce72c0d84e9301e4332
Narsi Nallamilli <narsi@accrualify.com>
2022-06-30 12:50:55 +0530

    remove comment

--- src/main/java/com/sopromadze/blogapi/controller/AlbumController.java
+++ src/main/java/com/sopromadze/blogapi/controller/AlbumController.java
@@ -43,7 +43,6 @@ public class AlbumController {
 
 	@ExceptionHandler(ResponseEntityErrorException.class)
 	public ResponseEntity<ApiResponse> handleExceptions(ResponseEntityErrorException exception) {
-		//add conflix change
 		return exception.getApiResponse();
 	}
 
